763892955a04dc188a615ea9cfd44c221698d44c
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-06-15 17:37:26 +0200

    added bluebox of main section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -13,8 +13,32 @@
 </script>
 
 <template>
-    <main class="d-flex align-items-center">
-        <h1 class="w-75 m-auto fw-bold"> --> Content goes here &lt;-- </h1>
+    <main class="">
+        <div class="products d-flex align-item-center">
+            <h2 class="fw-bold m-auto w-75"> --> Content goes here &lt;-- </h2>
+        </div>
+        <div class="blubox d-flex align-item-center justify-content-center">
+            <div class="element d-flex align-items-center px-5">
+                <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                <p>DIGITAL COMICS</p>
+            </div>
+            <div class="element d-flex align-items-center px-5">
+                <img src="../assets/img/buy-comics-merchandise.png" alt="">
+                <p>DC MERCHANDISE</p>
+            </div>
+            <div class="element d-flex align-items-center px-5">
+                <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+                <p>SUBSCRIPTION</p>
+            </div>
+            <div class="element d-flex align-items-center px-5">
+                <img src="../assets/img/buy-comics-shop-locator.png" alt="">
+                <p>COMIC SHOP LOCATOR</p>
+            </div>
+            <div class="element d-flex align-items-center px-5">
+                <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+                <p>DC POWER VISA</p>
+            </div>
+        </div>
     </main>
 </template>
 
@@ -22,12 +46,26 @@
     @use '../styles/partials/mixing.scss' as *;
     @use '../styles/partials/variables.scss' as *;
 
-    main{
+    .products{
         height: 200px;
         background-color: $bgMain;
     }
+    .blubox{
+        height: 250px;
+        background-color: $bgDc;
+    }
 
-    h1{
+    h2{
         color: $textWhite;
+    }
+
+    .element img{
+        height: 50px;
+    }
+
+    .element p{
+        color: $textWhite;
+        padding-left: 1rem;
+        margin-bottom: 0;
     }
 </style>
