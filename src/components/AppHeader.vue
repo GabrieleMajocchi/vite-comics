f2c25f1b728c32dc242fcf8770174d2e3e540042
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-06-15 17:01:32 +0200

    added elements and classes to the header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,7 +3,18 @@
         name: 'AppHeader',
         data() {
             return {
-                
+                links: [
+                    {ref: '#', name: 'Characters'},
+                    {ref: '#', name: 'Comics'},
+                    {ref: '#', name: 'Movies'},
+                    {ref: '#', name: 'TV'},
+                    {ref: '#', name: 'Games'},
+                    {ref: '#', name: 'Collectibles'},
+                    {ref: '#', name: 'Videos'},
+                    {ref: '#', name: 'Fans'},
+                    {ref: '#', name: 'News'},
+                    {ref: '#', name: 'Shop'},
+                ]
             }
         },
         methods: {
@@ -13,13 +24,48 @@
 </script>
 
 <template>
-    <header>
-        <h1>App Header</h1>
+    <header class="d-flex">
+        <div class="d-flex align-items-center m-auto justify-content-around my-3">
+            <img src="../assets/img/dc-logo.png" alt="">
+            <ul v-for="link in links" class="m-auto">
+                <li class="position-relative">
+                    <a class="fw-bold" :href="link.ref">{{(link.name).toUpperCase()}}</a>
+                    <div class="blubox"></div>
+                </li>
+            </ul>
+        </div>
     </header>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
     @use '../styles/partials/mixing.scss' as *;
     @use '../styles/partials/variables.scss' as *;
+
+    header{
+        width: 100vw;
+        background-color: $bgWhite;
+    }
+
+    img{
+        width: 80px;
+        margin-right: 500px;
+    }
+
+    a{
+        font-size: 0.9rem;
+        color: $textGrey;
+    }
+
+    li:hover .blubox{
+            display: block;
+        }
+    .blubox{
+        display: none;
+        background-color: $bgDc;
+        height: 5px;
+        width: 40px;
+        position: absolute;
+        bottom: -27px;
+    }
 
 </style>
