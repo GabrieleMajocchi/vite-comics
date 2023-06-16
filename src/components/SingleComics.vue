2f48ed8835981abbd7370580b378a471169c91c1
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-06-16 17:32:42 +0200

    created single comics component, with props form appmain to compile the varius comics

---
+++ src/components/SingleComics.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    name: 'NavLink',
+    props: {comicslist: Object,
+    },
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="comics">
+        <img :src="comicslist.thumb" :alt="comicslist.series">
+        <p class="comics-title">{{(comicslist.series).toUpperCase()}}</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    @use '../styles/partials/mixing.scss' as *;
+    @use '../styles/partials/variables.scss' as *;
+    
+    .comics{
+        width: calc(100% / 6 - 1rem);
+        color: $textWhite;
+        margin: 3rem 1rem 0 0;
+
+    }
+    .comics img{
+        width: 100%;
+        aspect-ratio: 1;
+        object-fit: cover;
+        object-position: top;
+    }
+</style>
